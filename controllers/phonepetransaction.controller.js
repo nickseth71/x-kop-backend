13a179dbd114d864807eefe42d349a831acd694c
Tayyab-Raza <traza9192@gmail.com>
2026-07-16 13:18:13 +0530

    fix: route fix model

--- controllers/phonepetransaction.controller.js
+++ controllers/phonepetransaction.controller.js
@@ -2,7 +2,7 @@ import crypto from "crypto"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { ApiError } from "../utils/ApiError.js"
-import PhonePeTransaction from "../models/phonepeTransaction.model.js"
+import PhonePeTransaction from "../models/phonepetransaction.model.js"
 import User from "../models/user.model.js"
 import {
   createSdkOrder,
